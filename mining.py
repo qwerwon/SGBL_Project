5033a4dc9229ce277710796cc70645031ecb103d
Ubuntu <ubuntu@ip-172-31-22-199.ap-northeast-2.compute.internal>
2018-08-31 08:13:54 +0000

    first

--- mining.py
+++ mining.py
@@ -12,10 +12,13 @@ from utxo import UTXOset, Vout
 class Mining(object):
     #class variables
     _MiningFlag = False
+    publicKey = 0
+    publicKey_ser =0
 
     def mineStart(self):
-        publicKey = Key._publicKey
-        publicKey_ser = publicKey.serialize(compressed=False)
+
+        self.publicKey = Key._publicKey
+        self.publicKey_ser = self.publicKey.serialize(compressed=False)
 
         if(self._MiningFlag):
             return True
@@ -23,14 +26,11 @@ class Mining(object):
         self.flagup()
 
         while(Mining._MiningFlag):
-            #warning for poinint class var
-            block_index = Blockchain._BlockHeight
-            previous_block = Blockchain.getLatestBlock().block_hash
             tx_set = []
 
-            # 아직 난이도는 고정값 사용, 지난 6개의 블록을 생성하는데 걸린 시간으로 다시 계산해야 함
-            # 이유는 모르겠지만 자꾸 keccak_hash(256bit)이 512bit으로 나온다
-            difficulty = 0x0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+
+            # coinbase transaction
+            tx_set.insert(0, self.generate_coinbase())
 
             # transaction의 priority는 아직 구현 못함
             # Block에 들어가는 transactino의 최대 크기는 아직 3개로 고정, 수정해야 함
@@ -45,49 +45,32 @@ class Mining(object):
 
             # 고정된 블록 보상인 12.5BTC만 일단 지급
 
-            # coinbase transaction 생성
-            in_num = 0
-            vin = []
-            out_num = 1
-            vout = [Vout(12.5, publicKey_ser)]
-            SumString = str(in_num) + str(vin) + str(out_num) + str(vout)
-            keccak_hash = keccak.new(digest_bits=256)
-            keccak_hash.update(SumString.encode('ascii'))
-            tx_id = keccak_hash.hexdigest().encode()
 
-            # coinbase transaction
-            tx_set.insert(0, Transaction(tx_id, in_num, vin, out_num, vout))
-
-            # Merkle root 생성
-            # 일단 tree를 만들지 않고, 모든 transaction의 hash를 다시 hash한 값을 merkle root로 간단하게 만듦, 수정 요망
-            keccak_hash = keccak.new(digest_bits=256)
-            for tx in tx_set:
-                keccak_hash.update(tx.tx_id)
-            merkle_root = keccak_hash.hexdigest()
-
-            blockData = str(previous_block) + str(merkle_root) + str(difficulty)
+            merkle_root = Block.create_merkle_root(tx_set)
+            previous_block = Blockchain.getLatestBlock().block_hash
+            blockData = str(previous_block) + str(merkle_root) + str(Block.difficulty)
 
             # PoW 호출
-            targetNonce = Mining.proofofwork(blockData, difficulty)
+            targetNonce = Mining.proofofwork(blockData, Block.difficulty)
 
             if (targetNonce == False):
                 print('Failed to get golden nonce')
                 continue
+            else:
+
+                #candidate블록을 생성한다.
+                tmp_block = Block.candidateblock(targetNonce, tx_set,merkle_root)
+
+                Blockchain.addBlock(tmp_block)
+                print('successfully mined new block#' + str(Blockchain._BlockHeight))
 
             # 채굴에 성공하면, 해당 블록에 포함되는transaction memoryPool에서 delete
             #for tx in tx_set:
             #   MemoryPool.delelte(tx.txid)
 
 
-            keccak_hash = keccak.new(digest_bits=256)
-            blockData = blockData + str(targetNonce)
-            keccak_hash.update(blockData.encode('ascii'))
-            block_hash = keccak_hash.hexdigest()
-            timestamp = time.time()
 
-            Blockchain.addBlock(
-                Block(block_index, block_hash, previous_block, merkle_root, difficulty, timestamp, targetNonce, tx_set))
-            print('successfully mined new block#' + str(Blockchain._BlockHeight))
+
 
             #Add to UTXOsets and myUTXOsets
             for tx in tx_set:
@@ -97,7 +80,7 @@ class Mining(object):
                     utxo_data = {"index": index, "address": address, "amount": vout.value}
                     utxo_data_en = json.dumps(utxo_data)
                     UTXOset._UTXOset.put(tx.tx_id, utxo_data_en.encode())
-                    if(vout.lock == publicKey_ser):
+                    if(vout.lock == self.publicKey_ser):
                         UTXOset._myUTXOset.put(tx.tx_id, utxo_data_en.encode())
 
             # 로그 확인을 위해서 3초 기다림
@@ -127,3 +110,20 @@ class Mining(object):
             nonce += 1
 
         return False
+
+
+
+    def generate_coinbase(self):
+        # coinbase transaction 생성
+        self.in_num = 0
+        self.vin = []
+        self.out_num = 1
+        self.vout = [Vout(12.5, self.publicKey_ser)]
+        SumString = str(self.in_num) + str(self.vin) + str(self.out_num) + str(self.vout)
+        keccak_hash = keccak.new(digest_bits=256)
+        keccak_hash.update(SumString.encode('ascii'))
+        tx_id = keccak_hash.hexdigest().encode()
+
+        return Transaction(tx_id, self.in_num, self.vin, self.out_num, self.vout)
+
+
